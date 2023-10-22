5244301fd6e09a3f6f05620f8d6a347226de9536
John Paul Songalia <paul.songalia@edufied.edu.vn>
2023-10-22 18:49:28 +0800

    fix cors

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173/",
+    "http://localhost:5173",
     "http://192.168.1.7:5173",
     "https://nlp-chat.netlify.app",
 ]
